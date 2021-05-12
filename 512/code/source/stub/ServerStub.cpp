ba7991d7b9c0e0074d0acc05d34efeff2b73cf7d
nx90 <2321296607@qq.com>
2021-05-12 17:58:46 +0800

    Delete 512 directory

--- 512/code/source/stub/ServerStub.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
- * Description: ServerStub
- * Author: lvyingchao 00596108
- * Create: 2021-05-11
- */
-
-#include "ServerStub.h"
